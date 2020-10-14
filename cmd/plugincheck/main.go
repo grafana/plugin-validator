c0a768d7ef696157d4d23626b1d9854ba40f1a04
Marcus Olsson <marcus.olsson@hey.com>
2020-10-14 13:32:23 +0200

    Download schema from Grafana repository

--- cmd/plugincheck/main.go
+++ cmd/plugincheck/main.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
 
 	"github.com/grafana/plugin-validator/pkg/grafana"
@@ -13,11 +16,35 @@ import (
 func main() {
 	var (
 		pluginURLFlag  = flag.String("url", "", "URL to the plugin")
-		schemaPathFlag = flag.String("schema", "./config/plugin.schema.json", "Path to the JSON Schema to validate against.")
+		schemaPathFlag = flag.String("schema", "", "Deprecated. Path to the JSON Schema to validate against.")
 		strictFlag     = flag.Bool("strict", false, "If set, plugincheck returns non-zero exit code for warnings")
 	)
 
 	flag.Parse()
+
+	if *schemaPathFlag != "" {
+		fmt.Println("Warning: The schema flag has been deprecated. plugincheck now downloads the schema from the Grafana repository.")
+	}
+
+	schemaFile, err := ioutil.TempFile("", "plugin_*.schema.json")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "couldn't create schema file")
+		os.Exit(1)
+	}
+	defer os.Remove(schemaFile.Name())
+
+	resp, err := http.Get("https://raw.githubusercontent.com/grafana/grafana/master/docs/sources/developers/plugins/plugin.schema.json")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "couldn't download plugin schema")
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(schemaFile, resp.Body)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "couldn't download plugin schema")
+		os.Exit(1)
+	}
 
 	if *pluginURLFlag == "" {
 		fmt.Fprintln(os.Stderr, "missing plugin url")
@@ -26,7 +53,7 @@ func main() {
 
 	client := grafana.NewClient()
 
-	_, result, err := plugin.Check(*pluginURLFlag, *schemaPathFlag, client)
+	_, result, err := plugin.Check(*pluginURLFlag, schemaFile.Name(), client)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
