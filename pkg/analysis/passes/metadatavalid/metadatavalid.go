00bcc5a2e32a5192188263969328f7dfed472acf
Brian Gann <bkgann@gmail.com>
2021-04-26 13:56:29 -0500

    fix output

--- pkg/analysis/passes/metadatavalid/metadatavalid.go
+++ pkg/analysis/passes/metadatavalid/metadatavalid.go
@@ -66,7 +66,7 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	for _, desc := range result.Errors() {
 		pass.Reportf(pass.AnalyzerName, invalidMetadata, fmt.Sprintf("plugin.json: %s: %s", desc.Field(), desc.Description()))
 	}
-	if len(result.Errors()) == 0 {
+	if len(result.Errors()) == 0 && invalidMetadata.ReportAll {
 		invalidMetadata.Severity = analysis.OK
 		pass.Reportf(pass.AnalyzerName, invalidMetadata, "plugin.json: metadata is valid")
 	}
