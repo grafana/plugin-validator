7b8567335af6034c9699596f2fb88886960ec699
Esteban Beltran <esteban.beltran@elastic.co>
2022-12-12 15:15:36 +0100

    Add pluginname test

---
+++ pkg/analysis/passes/pluginname/pluginname_test.go
@@ -0,0 +1,44 @@
+package pluginname
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/grafana/plugin-validator/pkg/analysis"
+	"github.com/grafana/plugin-validator/pkg/analysis/passes/metadata"
+	"github.com/grafana/plugin-validator/pkg/testpassinterceptor"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidPluginName(t *testing.T) {
+	const pluginId = "raintank-plugin-panel"
+	var interceptor testpassinterceptor.TestPassInterceptor
+	pass := &analysis.Pass{
+		RootDir: filepath.Join("./"),
+		ResultOf: map[*analysis.Analyzer]interface{}{
+			metadata.Analyzer: []byte(`{"ID": "` + pluginId + `", "name": "my plugin name", "info": {"logos": {"large": "img/logo.svg"}}}`),
+		},
+		Report: interceptor.ReportInterceptor(),
+	}
+
+	_, err := Analyzer.Run(pass)
+	require.NoError(t, err)
+	require.Len(t, interceptor.Diagnostics, 0)
+}
+
+func TestInvalidPluginName(t *testing.T) {
+	const pluginId = "raintank-plugin-panel"
+	var interceptor testpassinterceptor.TestPassInterceptor
+	pass := &analysis.Pass{
+		RootDir: filepath.Join("./"),
+		ResultOf: map[*analysis.Analyzer]interface{}{
+			metadata.Analyzer: []byte(`{"ID": "` + pluginId + `", "name": "` + pluginId + `", "info": {"logos": {"large": "img/logo.svg"}}}`),
+		},
+		Report: interceptor.ReportInterceptor(),
+	}
+
+	_, err := Analyzer.Run(pass)
+	require.NoError(t, err)
+	require.Len(t, interceptor.Diagnostics, 1)
+	require.Equal(t, interceptor.Diagnostics[0].Title, "plugin.json: plugin name should be human-friendly")
+}
