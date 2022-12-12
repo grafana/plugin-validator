77c476b3eaf346ca325b536ba9b25a7993b89e5f
Esteban Beltran <esteban.beltran@elastic.co>
2022-12-12 11:43:15 +0100

    Add tests for metadata

--- pkg/analysis/passes/metadata/metadata.go
+++ pkg/analysis/passes/metadata/metadata.go
@@ -1,6 +1,7 @@
 package metadata
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -24,6 +25,7 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	archiveDir := pass.ResultOf[archive.Analyzer].(string)
 
 	b, err := ioutil.ReadFile(filepath.Join(archiveDir, "plugin.json"))
+	fmt.Println("plugin.json path: ", filepath.Join(archiveDir, "plugin.json"))
 	if err != nil {
 		if os.IsNotExist(err) {
 			pass.ReportResult(pass.AnalyzerName, missingMetadata, "missing plugin.json", "A plugin.json file is required to describe the plugin.")
