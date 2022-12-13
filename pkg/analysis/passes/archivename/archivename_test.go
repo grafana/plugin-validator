a0c474ef2974ab54b90823c4777e2b93fbac8517
Esteban Beltran <esteban.beltran@elastic.co>
2022-12-13 14:13:30 +0100

    Fix merge conflict

--- pkg/analysis/passes/archivename/archivename_test.go
+++ pkg/analysis/passes/archivename/archivename_test.go
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-package archivename
-
-import (
-	"path/filepath"
-	"testing"
-
-	"github.com/grafana/plugin-validator/pkg/analysis"
-	"github.com/grafana/plugin-validator/pkg/analysis/passes/archive"
-	"github.com/grafana/plugin-validator/pkg/analysis/passes/metadata"
-	"github.com/grafana/plugin-validator/pkg/testpassinterceptor"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-)
-
-const pluginId = "test-plugin-panel"
-
-func TestRootDirNotDist(t *testing.T) {
-	var interceptor testpassinterceptor.TestPassInterceptor
-
-	pass := &analysis.Pass{
-		RootDir: filepath.Join("testdata", "dist-folder"),
-		ResultOf: map[*analysis.Analyzer]interface{}{
-			archive.Analyzer:  filepath.Join("testdata", "dist-folder", "dist"),
-			metadata.Analyzer: []byte(`{"ID": "` + pluginId + `"}`),
-		},
-		Report: interceptor.ReportInterceptor(),
-	}
-
-	_, err := Analyzer.Run(pass)
-	require.NoError(t, err)
-	require.Len(t, interceptor.Diagnostics, 2)
-	assert.Equal(t, interceptor.Diagnostics[0].Title, "Archive root directory named dist. It should contain a directory named test-plugin-panel")
-	assert.Equal(t, interceptor.Diagnostics[1].Title, "Archive should contain a directory named test-plugin-panel")
-}
-
-func TestRootSameAsPluginId(t *testing.T) {
-	var interceptor testpassinterceptor.TestPassInterceptor
-
-	pass := &analysis.Pass{
-		RootDir: filepath.Join("testdata", "not-plugin-id"),
-		ResultOf: map[*analysis.Analyzer]interface{}{
-			archive.Analyzer:  filepath.Join("testdata", "not-plugin-id", "the-files"),
-			metadata.Analyzer: []byte(`{"ID": "` + pluginId + `"}`),
-		},
-		Report: interceptor.ReportInterceptor(),
-	}
-
-	_, err := Analyzer.Run(pass)
-
-	require.NoError(t, err)
-	require.Len(t, interceptor.Diagnostics, 1)
-	assert.Equal(t, interceptor.Diagnostics[0].Title, "Archive should contain a directory named test-plugin-panel")
-}
-||||||| parent of 292cb93 (Add tests for archivename pass)
-=======
 package archivename
 
 import (
@@ -113,4 +57,3 @@ func TestRootSameAsPluginId(t *testing.T) {
 	require.Len(t, interceptor.Diagnostics, 1)
 	assert.Equal(t, interceptor.Diagnostics[0].Title, "Archive should contain a directory named test-plugin-panel")
 }
->>>>>>> 292cb93 (Add tests for archivename pass)
