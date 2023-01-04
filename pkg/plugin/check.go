f264edd26842203f1b628a9c994b94b502f82897
Esteban Beltran <esteban.beltran@elastic.co>
2023-01-04 16:38:42 +0100

    Remove duplicated code

--- pkg/plugin/check.go
+++ pkg/plugin/check.go
@@ -1,14 +1,10 @@
 package plugin
 
 import (
-	"archive/zip"
 	"bytes"
 	"encoding/json"
 	"errors"
-	"fmt"
-	"io"
 	"io/ioutil"
-	"os"
 	"path/filepath"
 	"strings"
 
@@ -62,7 +58,7 @@ func Check(archiveURL string, schemaPath string, private bool, client *grafana.C
 	}
 
 	// Extract the ZIP archive in a temporary directory.
-	archiveDir, cleanup, err := extractPlugin(bytes.NewReader(b))
+	archiveDir, cleanup, err := archivetool.ExtractPlugin(bytes.NewReader(b))
 	if err != nil {
 		return nil, nil, err
 	}
@@ -203,104 +199,4 @@ func usernameFromMetadata(metadata []byte) string {
 	}
 
 	return ""
-}
-
-func extractPlugin(body io.Reader) (string, func(), error) {
-	// Create a file for the zipball.
-	zipball, err := ioutil.TempFile("", "")
-	if err != nil {
-		return "", nil, err
-	}
-	defer zipball.Close()
-	defer os.Remove(zipball.Name())
-
-	if _, err := io.Copy(zipball, body); err != nil {
-		return "", nil, err
-	}
-
-	// Create a directory where we'll extract the archive.
-	output, err := ioutil.TempDir("", "")
-	if err != nil {
-		return "", nil, err
-	}
-
-	cleanup := func() {
-		os.RemoveAll(output)
-	}
-
-	if _, err := unzip(zipball.Name(), output); err != nil {
-		cleanup()
-		return "", nil, err
-	}
-
-	// infos, err := ioutil.ReadDir(output)
-	// if err != nil {
-	// 	cleanup()
-	// 	return "", nil, err
-	// }
-
-	// if len(infos) != 1 {
-	// 	cleanup()
-	// 	return "", nil, fmt.Errorf("unzip: expected 1 directory but got %d", len(infos))
-	// }
-
-	// pluginRoot := filepath.Join(output, infos[0].Name())
-
-	return output, cleanup, nil
-}
-
-func unzip(src string, dest string) ([]string, error) {
-	var filenames []string
-
-	r, err := zip.OpenReader(src)
-	if err != nil {
-		return filenames, err
-	}
-	defer r.Close()
-
-	for _, f := range r.File {
-		// Store filename/path for returning and using later on
-		fpath := filepath.Join(dest, f.Name)
-
-		// Check for ZipSlip. More Info: http://bit.ly/2MsjAWE
-		if !strings.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
-			return filenames, fmt.Errorf("%s: illegal file path", fpath)
-		}
-
-		filenames = append(filenames, fpath)
-
-		if f.FileInfo().IsDir() {
-			// Make Folder
-			if err = os.MkdirAll(fpath, os.ModePerm); err != nil {
-				return nil, err
-			}
-			continue
-		}
-
-		// Make File
-		if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
-			return filenames, err
-		}
-
-		outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-		if err != nil {
-			return filenames, err
-		}
-
-		rc, err := f.Open()
-		if err != nil {
-			return filenames, err
-		}
-
-		_, err = io.Copy(outFile, rc)
-
-		// Close the file without defer to close before next iteration of loop
-		outFile.Close()
-		rc.Close()
-
-		if err != nil {
-			return filenames, err
-		}
-	}
-	return filenames, nil
 }
