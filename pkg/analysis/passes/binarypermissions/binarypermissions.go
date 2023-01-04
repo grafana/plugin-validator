500d055072677a03c09bb8a2a0c1cf42472e7d33
Esteban Beltran <esteban.beltran@elastic.co>
2023-01-04 15:10:57 +0100

    Add validator code for binary permissions check

---
+++ pkg/analysis/passes/binarypermissions/binarypermissions.go
@@ -0,0 +1,98 @@
+package binarypermissions
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+
+	"github.com/grafana/plugin-validator/pkg/analysis"
+	"github.com/grafana/plugin-validator/pkg/analysis/passes/archive"
+	"github.com/grafana/plugin-validator/pkg/analysis/passes/metadata"
+)
+
+var (
+	binaryExecutableFound       = &analysis.Rule{Name: "binary-executable-found", Severity: analysis.Error}
+	binaryExecutablePermissions = &analysis.Rule{Name: "binary-executable-permissions", Severity: analysis.Error}
+)
+
+var REQUIRED_PERMISSIONS = fs.FileMode.Perm(0755)
+
+var Analyzer = &analysis.Analyzer{
+	Name:     "archivename",
+	Requires: []*analysis.Analyzer{metadata.Analyzer, archive.Analyzer},
+	Run:      run,
+	Rules:    []*analysis.Rule{binaryExecutableFound},
+}
+
+var binarySuffixes = []string{
+	"_linux_amd64",
+	"_linux_arm64",
+	"_darwin_amd64",
+	"_darwin_arm64",
+	"_windows_amd64.exe",
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	archiveDir := pass.ResultOf[archive.Analyzer].(string)
+	metadataBody := pass.ResultOf[metadata.Analyzer].([]byte)
+
+	var data metadata.Metadata
+	if err := json.Unmarshal(metadataBody, &data); err != nil {
+		return nil, err
+	}
+
+	if data.Executable == "" {
+		binaryExecutableFound.Severity = analysis.OK
+		pass.ReportResult(pass.AnalyzerName, binaryExecutableFound, "No executable defined in plugin.json", "")
+		return nil, nil
+	}
+
+	executable := data.Executable
+	executableParentDir := filepath.Join(archiveDir, filepath.Dir(executable))
+	executableName := filepath.Base(executable)
+
+	var foundBinaries = []string{}
+	for _, suffix := range binarySuffixes {
+		binaryPath := filepath.Join(executableParentDir, executableName+suffix)
+		if _, err := os.Stat(binaryPath); os.IsNotExist(err) {
+			continue
+		}
+		foundBinaries = append(foundBinaries, binaryPath)
+	}
+
+	if len(foundBinaries) == 0 {
+		pass.ReportResult(pass.AnalyzerName,
+			binaryExecutableFound,
+			fmt.Sprintf("No binary found for `executable` %s defined in plugin.json", executable),
+			fmt.Sprintf("You defined an executable %s but it could not be found for any of the supported architectures", executable))
+		return nil, nil
+	}
+
+	for _, binaryPath := range foundBinaries {
+
+		// skip windows executables
+		if filepath.Ext(binaryPath) == ".exe" {
+			continue
+		}
+
+		fileInfo, err := os.Stat(binaryPath)
+		if err != nil {
+			pass.ReportResult(pass.AnalyzerName,
+				binaryExecutablePermissions,
+				fmt.Sprintf("Could not read permissions for executable %s", executable),
+				fmt.Sprintf("Could not read the file %s. This could be too restrictive in your binary files or your zip file is corrupted", executable))
+		}
+
+		filePermissions := fileInfo.Mode().Perm()
+		if filePermissions != REQUIRED_PERMISSIONS {
+			pass.ReportResult(pass.AnalyzerName,
+				binaryExecutablePermissions,
+				fmt.Sprintf("Permissions for binary executable %s are incorrect (%04o found).", executable, filePermissions),
+				fmt.Sprintf("The binary file %s must have exact permissions %04o (%s).", executable, REQUIRED_PERMISSIONS, REQUIRED_PERMISSIONS))
+		}
+	}
+
+	return nil, nil
+}
---
+++ pkg/analysis/passes/binarypermissions/binarypermissions_test.go
@@ -0,0 +1,105 @@
+package binarypermissions
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/grafana/plugin-validator/pkg/analysis"
+	"github.com/grafana/plugin-validator/pkg/analysis/passes/archive"
+	"github.com/grafana/plugin-validator/pkg/analysis/passes/metadata"
+	"github.com/grafana/plugin-validator/pkg/testpassinterceptor"
+	"github.com/stretchr/testify/require"
+)
+
+const pluginId = "test-plugin-panel"
+
+func TestBinaryFoundWithCorrectPermissions(t *testing.T) {
+	var interceptor testpassinterceptor.TestPassInterceptor
+	pluginJsonContent := []byte(`{
+		"id": "` + pluginId + `",
+		"type": "panel",
+		"executable": "test-plugin-panel"
+	}`)
+	pass := &analysis.Pass{
+		RootDir: filepath.Join("./"),
+		ResultOf: map[*analysis.Analyzer]interface{}{
+			metadata.Analyzer: pluginJsonContent,
+			archive.Analyzer:  filepath.Join("testdata", "correct-permissions"),
+		},
+		Report: interceptor.ReportInterceptor(),
+	}
+
+	_, err := Analyzer.Run(pass)
+	require.NoError(t, err)
+
+	require.Len(t, interceptor.Diagnostics, 0)
+}
+
+func TestBinaryNotFound(t *testing.T) {
+	var interceptor testpassinterceptor.TestPassInterceptor
+	pluginJsonContent := []byte(`{
+		"id": "` + pluginId + `",
+		"type": "panel",
+		"executable": "test-plugin-panel"
+	}`)
+	pass := &analysis.Pass{
+		RootDir: filepath.Join("./"),
+		ResultOf: map[*analysis.Analyzer]interface{}{
+			metadata.Analyzer: pluginJsonContent,
+			archive.Analyzer:  filepath.Join("testdata", "no-binary"),
+		},
+		Report: interceptor.ReportInterceptor(),
+	}
+
+	_, err := Analyzer.Run(pass)
+	require.NoError(t, err)
+
+	require.Len(t, interceptor.Diagnostics, 1)
+	require.Equal(t, interceptor.Diagnostics[0].Title, "No binary found for `executable` test-plugin-panel defined in plugin.json")
+}
+
+func TestBinaryNotFoundNested(t *testing.T) {
+	var interceptor testpassinterceptor.TestPassInterceptor
+	pluginJsonContent := []byte(`{
+		"id": "` + pluginId + `",
+		"type": "panel",
+		"executable": "binaries/test-plugin-panel"
+	}`)
+	pass := &analysis.Pass{
+		RootDir: filepath.Join("./"),
+		ResultOf: map[*analysis.Analyzer]interface{}{
+			metadata.Analyzer: pluginJsonContent,
+			archive.Analyzer:  filepath.Join("testdata", "correct-permissions-nested"),
+		},
+		Report: interceptor.ReportInterceptor(),
+	}
+
+	_, err := Analyzer.Run(pass)
+	require.NoError(t, err)
+
+	require.Len(t, interceptor.Diagnostics, 0)
+}
+
+func TestBinaryIncorrectPermissions(t *testing.T) {
+	var interceptor testpassinterceptor.TestPassInterceptor
+	pluginJsonContent := []byte(`{
+		"id": "` + pluginId + `",
+		"type": "panel",
+		"executable": "test-plugin-panel"
+	}`)
+	pass := &analysis.Pass{
+		RootDir: filepath.Join("./"),
+		ResultOf: map[*analysis.Analyzer]interface{}{
+			metadata.Analyzer: pluginJsonContent,
+			archive.Analyzer:  filepath.Join("testdata", "wrong-permissions"),
+		},
+		Report: interceptor.ReportInterceptor(),
+	}
+
+	_, err := Analyzer.Run(pass)
+	require.NoError(t, err)
+
+	require.Len(t, interceptor.Diagnostics, 2)
+	require.Equal(t, interceptor.Diagnostics[0].Title, "Permissions for binary executable test-plugin-panel are incorrect (0777 found).")
+	require.Equal(t, interceptor.Diagnostics[1].Title, "Permissions for binary executable test-plugin-panel are incorrect (0711 found).")
+}
