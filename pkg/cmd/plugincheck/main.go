b3754dea56ecb527c74153148d27082ae0de9fec
Esteban Beltran <esteban.beltran@elastic.co>
2023-01-05 14:20:34 +0100

    Add ascii deprecated message

--- pkg/cmd/plugincheck/main.go
+++ pkg/cmd/plugincheck/main.go
@@ -14,6 +14,26 @@ import (
 )
 
 func main() {
+
+	fmt.Println(`
+     888                                               888                888
+     888                                               888                888
+     888                                               888                888
+ .d88888 .d88b. 88888b. 888d888 .d88b.  .d8888b 8888b. 888888 .d88b.  .d88888
+d88" 888d8P  Y8b888 "88b888P"  d8P  Y8bd88P"       "88b888   d8P  Y8bd88" 888
+888  88888888888888  888888    88888888888     .d888888888   88888888888  888
+Y88b 888Y8b.    888 d88P888    Y8b.    Y88b.   888  888Y88b. Y8b.    Y88b 888
+ "Y88888 "Y8888 88888P" 888     "Y8888  "Y8888P"Y888888 "Y888 "Y8888  "Y88888
+                888
+                888
+                888
+
+	**pluginvalidator V1 is deprecated and you should not use it.
+
+	Use V2 instead: plugincheck2
+
+	To install install it see README https://github.com/grafana/plugin-validator`)
+	fmt.Println()
 	var (
 		strictFlag  = flag.Bool("strict", false, "If set, plugincheck returns non-zero exit code for warnings")
 		privateFlag = flag.Bool("private", false, "If set, plugincheck reports private signature check error as warning")
