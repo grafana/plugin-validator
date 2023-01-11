023f66dd459eef6548ce254b99d2292c06988050
Esteban Beltran <academo@users.noreply.github.com>
2023-01-11 08:46:01 +0000

    Add validation for plugin version (#53)

---
+++ pkg/analysis/passes/version/version.go
@@ -0,0 +1,114 @@
+package version
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/grafana/plugin-validator/pkg/analysis"
+	"github.com/grafana/plugin-validator/pkg/analysis/passes/metadata"
+	"github.com/hashicorp/go-version"
+)
+
+var (
+	wrongPluginVersion = &analysis.Rule{Name: "wrong-plugin-version", Severity: analysis.Error}
+)
+
+type PluginStatus struct {
+	Status  string `json:"status"`
+	Slug    string `json:"slug"`
+	Version string `json:"version"`
+}
+
+var Analyzer = &analysis.Analyzer{
+	Name:     "version",
+	Requires: []*analysis.Analyzer{metadata.Analyzer},
+	Run:      run,
+	Rules:    []*analysis.Rule{wrongPluginVersion},
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	metadataBody := pass.ResultOf[metadata.Analyzer].([]byte)
+
+	var data metadata.Metadata
+	if err := json.Unmarshal(metadataBody, &data); err != nil {
+		return nil, err
+	}
+
+	// try to parse the submission version
+	pluginSubmissionVersion := data.Info.Version
+	parsedSubmissionVersion, err := version.NewVersion(pluginSubmissionVersion)
+	if err != nil {
+		pass.ReportResult(pass.AnalyzerName,
+			wrongPluginVersion,
+			fmt.Sprintf("Plugin version \"%s\" is invalid.", pluginSubmissionVersion),
+			fmt.Sprintf("Could not parse plugin version \"%s\". Please use a valid semver version for your plugin. See https://semver.org/.", pluginSubmissionVersion))
+		return nil, nil
+	}
+
+	context, cancelContext := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancelContext()
+
+	pluginStatus, err := getPluginDataFromGrafanaCom(context, data.ID)
+	if err != nil {
+		// in case of any error getting the online status, skip this check
+		return nil, nil
+	}
+	grafanaComVersion := pluginStatus.Version
+	parsedGrafanaVersion, err := version.NewVersion(grafanaComVersion)
+	if err != nil {
+		// in case of any error parsing the online status, skip this check
+		return nil, nil
+	}
+
+	if !parsedSubmissionVersion.GreaterThan(parsedGrafanaVersion) {
+		pass.ReportResult(pass.AnalyzerName,
+			wrongPluginVersion,
+			fmt.Sprintf("Plugin version %s is invalid.", pluginSubmissionVersion),
+			fmt.Sprintf("The submitted plugin version %s is not greater than the latest published version %s on grafana.com.", pluginSubmissionVersion, grafanaComVersion),
+		)
+		return nil, nil
+	} else if wrongPluginVersion.ReportAll {
+		wrongPluginVersion.Severity = analysis.OK
+		pass.ReportResult(pass.AnalyzerName,
+			wrongPluginVersion,
+			fmt.Sprintf("Valid Plugin version %s", pluginSubmissionVersion),
+			"")
+	}
+
+	return nil, nil
+}
+
+func getPluginDataFromGrafanaCom(context context.Context, pluginId string) (*PluginStatus, error) {
+	pluginUrl := fmt.Sprintf("https://grafana.com/api/plugins/%s?version=latest", pluginId)
+	// fetch content for pluginUrl
+	request, err := http.NewRequestWithContext(context, "GET", pluginUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Add("Accept", "application/json")
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	// 404 = the plugin is not yet published
+	if response.StatusCode == http.StatusNotFound {
+		return nil, errors.New("plugin not found")
+	}
+
+	// != 200 = something went wrong. We can't check the plugin
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("wrong status code, expected 200 got %d", response.StatusCode)
+	}
+
+	status := PluginStatus{}
+	if err := json.NewDecoder(response.Body).Decode(&status); err != nil {
+		return nil, err
+	}
+	return &status, nil
+}
---
+++ pkg/analysis/passes/version/version_test.go
@@ -0,0 +1,168 @@
+package version
+
+import (
+	"fmt"
+	"net/http"
+	"path/filepath"
+	"testing"
+
+	"github.com/grafana/plugin-validator/pkg/analysis"
+	"github.com/grafana/plugin-validator/pkg/analysis/passes/metadata"
+	"github.com/grafana/plugin-validator/pkg/testpassinterceptor"
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/require"
+)
+
+const testPluginId = "test-plugin-panel"
+
+func getMockVersionResponse(id string, version string) string {
+	content := fmt.Sprintf(`
+	{
+  	"status": "active",
+  	"id": 31,
+  	"typeId": 3,
+  	"typeName": "Panel",
+  	"typeCode": "panel",
+  	"slug": "%s",
+  	"name": "Clock",
+  	"description": "Clock panel for grafana",
+  	"version": "%s",
+  	"orgName": "Grafana Labs",
+  	"orgSlug": "grafana",
+  	"orgUrl": "https://grafana.org",
+  	"url": "https://github.com/grafana/clock-panel/",
+  	"createdAt": "2016-03-31T13:09:33.000Z",
+  	"updatedAt": "2023-01-04T10:24:26.000Z"
+  }
+	`, id, version)
+	return content
+}
+
+func setupTestAnalyzer(pluginSubmissionVersion string, pluginGrafanaComVersion string) (*analysis.Pass, *testpassinterceptor.TestPassInterceptor, func()) {
+	var interceptor testpassinterceptor.TestPassInterceptor
+
+	if pluginGrafanaComVersion != "" {
+		httpmock.Activate()
+
+		responseContent := getMockVersionResponse(testPluginId, pluginGrafanaComVersion)
+		responseCode := http.StatusOK
+
+		// mock grafana.com response
+		pluginUrl := fmt.Sprintf("https://grafana.com/api/plugins/%s?version=latest", testPluginId)
+		httpmock.RegisterResponder("GET", pluginUrl,
+			httpmock.NewStringResponder(responseCode, responseContent))
+	}
+
+	pluginJsonContent := []byte(`{
+		"id": "` + testPluginId + `",
+		"type": "panel",
+		"executable": "test-plugin-panel",
+		"info": {
+			"version": "` + pluginSubmissionVersion + `"
+		}
+	}`)
+
+	pass := &analysis.Pass{
+		RootDir: filepath.Join("./"),
+		ResultOf: map[*analysis.Analyzer]interface{}{
+			metadata.Analyzer: pluginJsonContent,
+		},
+		Report: interceptor.ReportInterceptor(),
+	}
+	return pass, &interceptor, httpmock.DeactivateAndReset
+}
+
+// an unpublished plugin should simply skip this check by
+// returning nil and no errors
+func TestUnpublishedPlugin(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	pass, interceptor, cleanup := setupTestAnalyzer("1.0.0", "")
+	defer cleanup()
+
+	responseContent := `{
+  	"code": "NotFound",
+  	"message": "plugin not found",
+  	"requestId": "mock-test-id"
+	}`
+	responseCode := http.StatusNotFound
+
+	// mock grafana.com response
+	pluginUrl := fmt.Sprintf("https://grafana.com/api/plugins/%s?version=latest", testPluginId)
+	httpmock.RegisterResponder("GET", pluginUrl,
+		httpmock.NewStringResponder(responseCode, responseContent))
+
+	analyzerResult, err := Analyzer.Run(pass)
+	require.NoError(t, err)
+
+	require.Len(t, httpmock.GetCallCountInfo(), 1)
+	require.Len(t, interceptor.Diagnostics, 0)
+	require.Nil(t, analyzerResult)
+}
+
+func TestHigherVersion(t *testing.T) {
+
+	pluginSubmissionVersion := "1.0.1" // version in submitted plugin.json
+	pluginGrafanaComVersion := "1.0.0" // version in grafana.com
+	pass, interceptor, cleanup := setupTestAnalyzer(pluginSubmissionVersion, pluginGrafanaComVersion)
+	defer cleanup()
+
+	analyzerResult, err := Analyzer.Run(pass)
+	require.NoError(t, err)
+
+	require.Len(t, httpmock.GetCallCountInfo(), 1)
+	require.Len(t, interceptor.Diagnostics, 0)
+	require.Nil(t, analyzerResult)
+}
+
+func TestSameVersion(t *testing.T) {
+
+	pluginSubmissionVersion := "1.0.0" // version in submitted plugin.json
+	pluginGrafanaComVersion := "1.0.0" // version in grafana.com
+	pass, interceptor, cleanup := setupTestAnalyzer(pluginSubmissionVersion, pluginGrafanaComVersion)
+	defer cleanup()
+
+	analyzerResult, err := Analyzer.Run(pass)
+	require.NoError(t, err)
+
+	require.Len(t, httpmock.GetCallCountInfo(), 1)
+	require.Len(t, interceptor.Diagnostics, 1)
+	require.Equal(t, "Plugin version 1.0.0 is invalid.", interceptor.Diagnostics[0].Title)
+	require.Equal(t, "The submitted plugin version 1.0.0 is not greater than the latest published version 1.0.0 on grafana.com.", interceptor.Diagnostics[0].Detail)
+	require.Nil(t, analyzerResult)
+}
+
+func TestLowerVersion(t *testing.T) {
+
+	pluginSubmissionVersion := "0.9.6" // version in submitted plugin.json
+	pluginGrafanaComVersion := "1.0.0" // version in grafana.com
+	pass, interceptor, cleanup := setupTestAnalyzer(pluginSubmissionVersion, pluginGrafanaComVersion)
+	defer cleanup()
+
+	analyzerResult, err := Analyzer.Run(pass)
+	require.NoError(t, err)
+
+	require.Len(t, httpmock.GetCallCountInfo(), 1)
+	require.Len(t, interceptor.Diagnostics, 1)
+	require.Equal(t, "Plugin version 0.9.6 is invalid.", interceptor.Diagnostics[0].Title)
+	require.Equal(t, "The submitted plugin version 0.9.6 is not greater than the latest published version 1.0.0 on grafana.com.", interceptor.Diagnostics[0].Detail)
+	require.Nil(t, analyzerResult)
+}
+
+func TestWrongVersionFormat(t *testing.T) {
+
+	pluginSubmissionVersion := "first-one" // version in submitted plugin.json
+	pluginGrafanaComVersion := "1.0.0"     // version in grafana.com
+	pass, interceptor, cleanup := setupTestAnalyzer(pluginSubmissionVersion, pluginGrafanaComVersion)
+	defer cleanup()
+
+	analyzerResult, err := Analyzer.Run(pass)
+	require.NoError(t, err)
+
+	require.Len(t, httpmock.GetCallCountInfo(), 1)
+	require.Len(t, interceptor.Diagnostics, 1)
+	require.Equal(t, "Plugin version \"first-one\" is invalid.", interceptor.Diagnostics[0].Title)
+	require.Equal(t, "Could not parse plugin version \"first-one\". Please use a valid semver version for your plugin. See https://semver.org/.", interceptor.Diagnostics[0].Detail)
+	require.Nil(t, analyzerResult)
+}
