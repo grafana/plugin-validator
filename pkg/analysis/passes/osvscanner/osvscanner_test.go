f452ed6954a8991581eac19c333f4566cff86f50
Brian Gann <bkgann@gmail.com>
2023-02-14 09:22:53 -0500

    update tests and refactor to process multiple lock files

--- pkg/analysis/passes/osvscanner/osvscanner_test.go
+++ pkg/analysis/passes/osvscanner/osvscanner_test.go
@@ -10,6 +10,19 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func reportAll(a *analysis.Analyzer) {
+	for _, r := range a.Rules {
+		r.ReportAll = true
+	}
+}
+
+func undoReportAll(a *analysis.Analyzer) {
+	for _, r := range a.Rules {
+		r.ReportAll = false
+	}
+}
+
+// TestCanRunScanner
 func TestCanRunScanner(t *testing.T) {
 	var interceptor testpassinterceptor.TestPassInterceptor
 	pass := &analysis.Pass{
@@ -21,11 +34,33 @@ func TestCanRunScanner(t *testing.T) {
 	}
 	_, err := Analyzer.Run(pass)
 	require.NoError(t, err)
-
-	require.Len(t, interceptor.Diagnostics, 1)
-	require.NotEqual(t, "error running osv-scanner", interceptor.Diagnostics[0].Title)
+	require.Len(t, interceptor.Diagnostics, 0)
 }
 
+// TestCanRunScannerReportAll
+func TestCanRunScannerReportAll(t *testing.T) {
+	var interceptor testpassinterceptor.TestPassInterceptor
+	pass := &analysis.Pass{
+		RootDir: filepath.Join("./"),
+		ResultOf: map[*analysis.Analyzer]interface{}{
+			archive.Analyzer: filepath.Join("testdata", "golang", "none"),
+		},
+		Report: interceptor.ReportInterceptor(),
+	}
+	// Turn on ReportAll for all rules, then turn it back off at the end of the test
+	reportAll(Analyzer)
+	t.Cleanup(func() {
+		undoReportAll(Analyzer)
+	})
+	_, err := Analyzer.Run(pass)
+	require.NoError(t, err)
+	require.Len(t, interceptor.Diagnostics, 3)
+	require.Equal(t, "Binary for osv-scanner was found in PATH", interceptor.Diagnostics[0].Title)
+	require.Equal(t, "osv-scanner successfully ran", interceptor.Diagnostics[1].Title)
+	require.Equal(t, "osv-scanner passed", interceptor.Diagnostics[2].Title)
+}
+
+// TestEmptyResults
 func TestEmptyResults(t *testing.T) {
 	var interceptor testpassinterceptor.TestPassInterceptor
 	pass := &analysis.Pass{
@@ -37,11 +72,31 @@ func TestEmptyResults(t *testing.T) {
 	}
 	_, err := Analyzer.Run(pass)
 	require.NoError(t, err)
-
-	require.Len(t, interceptor.Diagnostics, 1)
-	require.Equal(t, "osv-scanner successfully ran", interceptor.Diagnostics[0].Title)
+	require.Len(t, interceptor.Diagnostics, 0)
 }
 
+// TestEmptyResultsReportAll
+func TestEmptyResultsReportAll(t *testing.T) {
+	var interceptor testpassinterceptor.TestPassInterceptor
+	pass := &analysis.Pass{
+		RootDir: filepath.Join("./"),
+		ResultOf: map[*analysis.Analyzer]interface{}{
+			archive.Analyzer: filepath.Join("testdata", "golang", "none"),
+		},
+		Report: interceptor.ReportInterceptor(),
+	}
+	// Turn on ReportAll for all rules, then turn it back off at the end of the test
+	reportAll(Analyzer)
+	t.Cleanup(func() {
+		undoReportAll(Analyzer)
+	})
+	_, err := Analyzer.Run(pass)
+	require.NoError(t, err)
+	require.Len(t, interceptor.Diagnostics, 3)
+	require.Equal(t, "osv-scanner passed", interceptor.Diagnostics[2].Title)
+}
+
+// TestNoIssueResults
 func TestNoIssueResults(t *testing.T) {
 	var interceptor testpassinterceptor.TestPassInterceptor
 	pass := &analysis.Pass{
@@ -53,12 +108,32 @@ func TestNoIssueResults(t *testing.T) {
 	}
 	_, err := Analyzer.Run(pass)
 	require.NoError(t, err)
-
-	require.Len(t, interceptor.Diagnostics, 1)
-	require.Equal(t, "osv-scanner successfully ran", interceptor.Diagnostics[0].Title)
+	require.Len(t, interceptor.Diagnostics, 0)
 }
 
-// TestCriticalSeverityResults checks for a critical severity issue
+// TestNoIssueResultsReportAll
+func TestNoIssueResultsReportAll(t *testing.T) {
+	var interceptor testpassinterceptor.TestPassInterceptor
+	pass := &analysis.Pass{
+		RootDir: filepath.Join("./"),
+		ResultOf: map[*analysis.Analyzer]interface{}{
+			archive.Analyzer: filepath.Join("testdata", "node", "none"),
+		},
+		Report: interceptor.ReportInterceptor(),
+	}
+	// Turn on ReportAll for all rules, then turn it back off at the end of the test
+	reportAll(Analyzer)
+	t.Cleanup(func() {
+		undoReportAll(Analyzer)
+	})
+	_, err := Analyzer.Run(pass)
+	require.NoError(t, err)
+
+	require.Len(t, interceptor.Diagnostics, 3)
+	require.Equal(t, "osv-scanner passed", interceptor.Diagnostics[2].Title)
+}
+
+// TestCriticalSeverityResults
 func TestCriticalSeverityResults(t *testing.T) {
 	var interceptor testpassinterceptor.TestPassInterceptor
 	pass := &analysis.Pass{
@@ -70,14 +145,36 @@ func TestCriticalSeverityResults(t *testing.T) {
 	}
 	_, err := Analyzer.Run(pass)
 	require.NoError(t, err)
-
 	require.Len(t, interceptor.Diagnostics, 2)
 	require.Equal(t, "osv-scanner detected a critical severity issue", interceptor.Diagnostics[0].Title)
 	require.Equal(t, "osv-scanner detected critical severity issues", interceptor.Diagnostics[1].Title)
 }
 
-// TestHighSeverityResults checks for a high severity issue
-func TestHighSeverityResults(t *testing.T) {
+// TestCriticalSeverityResultsReportAll checks for a critical severity issue
+func TestCriticalSeverityResultsReportAll(t *testing.T) {
+	var interceptor testpassinterceptor.TestPassInterceptor
+	pass := &analysis.Pass{
+		RootDir: filepath.Join("./"),
+		ResultOf: map[*analysis.Analyzer]interface{}{
+			archive.Analyzer: filepath.Join("testdata", "node", "critical"),
+		},
+		Report: interceptor.ReportInterceptor(),
+	}
+	// Turn on ReportAll for all rules, then turn it back off at the end of the test
+	reportAll(Analyzer)
+	t.Cleanup(func() {
+		undoReportAll(Analyzer)
+	})
+	_, err := Analyzer.Run(pass)
+	require.NoError(t, err)
+	require.Len(t, interceptor.Diagnostics, 4)
+	require.Equal(t, "osv-scanner detected a critical severity issue", interceptor.Diagnostics[2].Title)
+	require.Equal(t, "osv-scanner detected critical severity issues", interceptor.Diagnostics[3].Title)
+}
+
+// TestHighSeverityResultsReportAll
+// high severity does not report any output, unless the report all option is enabled
+func TestHighSeverityResultsReportAll(t *testing.T) {
 	var interceptor testpassinterceptor.TestPassInterceptor
 	pass := &analysis.Pass{
 		RootDir: filepath.Join("./"),
@@ -86,16 +183,21 @@ func TestHighSeverityResults(t *testing.T) {
 		},
 		Report: interceptor.ReportInterceptor(),
 	}
+	// Turn on ReportAll for all rules, then turn it back off at the end of the test
+	reportAll(Analyzer)
+	t.Cleanup(func() {
+		undoReportAll(Analyzer)
+	})
 	_, err := Analyzer.Run(pass)
 	require.NoError(t, err)
-
-	require.Len(t, interceptor.Diagnostics, 2)
-	require.Equal(t, "osv-scanner detected a high severity issue", interceptor.Diagnostics[0].Title)
-	require.Equal(t, "osv-scanner detected high severity issues", interceptor.Diagnostics[1].Title)
+	require.Len(t, interceptor.Diagnostics, 4)
+	require.Equal(t, "osv-scanner detected a high severity issue", interceptor.Diagnostics[2].Title)
+	require.Equal(t, "osv-scanner detected high severity issues", interceptor.Diagnostics[3].Title)
 }
 
-// TestModerateSeverityResults checks for a moderate severity issue
-func TestModerateSeverityResults(t *testing.T) {
+// TestModerateSeverityResultsReportAll checks for a moderate severity issue
+// moderate severity does not report any output, unless the report all option is enabled
+func TestModerateSeverityResultsReportAll(t *testing.T) {
 	var interceptor testpassinterceptor.TestPassInterceptor
 	pass := &analysis.Pass{
 		RootDir: filepath.Join("./"),
@@ -104,16 +206,20 @@ func TestModerateSeverityResults(t *testing.T) {
 		},
 		Report: interceptor.ReportInterceptor(),
 	}
+	// Turn on ReportAll for all rules, then turn it back off at the end of the test
+	reportAll(Analyzer)
+	t.Cleanup(func() {
+		undoReportAll(Analyzer)
+	})
 	_, err := Analyzer.Run(pass)
 	require.NoError(t, err)
-
-	require.Len(t, interceptor.Diagnostics, 2)
-	require.Equal(t, "osv-scanner detected a moderate severity issue", interceptor.Diagnostics[0].Title)
-	require.Equal(t, "osv-scanner detected moderate severity issues", interceptor.Diagnostics[1].Title)
+	require.Len(t, interceptor.Diagnostics, 4)
+	require.Equal(t, "osv-scanner detected a moderate severity issue", interceptor.Diagnostics[2].Title)
+	require.Equal(t, "osv-scanner detected moderate severity issues", interceptor.Diagnostics[3].Title)
 }
 
-// TestLowSeverityResults checks for a low severity issue
-func TestLowSeverityResults(t *testing.T) {
+// TestLowSeverityResultsReportAll checks for a low severity issue
+func TestLowSeverityResultsReportAll(t *testing.T) {
 	var interceptor testpassinterceptor.TestPassInterceptor
 	pass := &analysis.Pass{
 		RootDir: filepath.Join("./"),
@@ -122,10 +228,14 @@ func TestLowSeverityResults(t *testing.T) {
 		},
 		Report: interceptor.ReportInterceptor(),
 	}
+	// Turn on ReportAll for all rules, then turn it back off at the end of the test
+	reportAll(Analyzer)
+	t.Cleanup(func() {
+		undoReportAll(Analyzer)
+	})
 	_, err := Analyzer.Run(pass)
 	require.NoError(t, err)
-
-	require.Len(t, interceptor.Diagnostics, 4)
-	require.Equal(t, "osv-scanner detected a low severity issue", interceptor.Diagnostics[0].Title)
-	require.Equal(t, "osv-scanner detected low severity issues", interceptor.Diagnostics[3].Title)
+	require.Len(t, interceptor.Diagnostics, 6)
+	require.Equal(t, "osv-scanner detected a low severity issue", interceptor.Diagnostics[2].Title)
+	require.Equal(t, "osv-scanner detected low severity issues", interceptor.Diagnostics[5].Title)
 }
