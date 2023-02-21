be461b6363a41d206e4e57b4ee49f5d21e66ecca
Esteban Beltran <academo@users.noreply.github.com>
2023-02-21 13:35:30 +0100

    Add gomanifest validation (#68)

---
+++ pkg/analysis/passes/gomanifest/testdata/correct-manifest/src/pkg/main.go
@@ -0,0 +1,5 @@
+package src
+
+func main() {
+	panic("do not include this. this is a test file only")
+}
---
+++ pkg/analysis/passes/gomanifest/testdata/incorrect-manifest/src/pkg/main.go
@@ -0,0 +1,6 @@
+package src
+
+func main() {
+	panic("do not include this. this is a test file only")
+	// this is a different line
+}
