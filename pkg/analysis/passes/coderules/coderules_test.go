666297a10cb9a668ee0657ba0c8e852182b3d158
Esteban Beltran <esteban.beltran@elastic.co>
2023-02-23 15:02:17 +0100

    Account for allowed GF_PLUGIN variables

--- pkg/analysis/passes/coderules/coderules_test.go
+++ pkg/analysis/passes/coderules/coderules_test.go
@@ -24,10 +24,31 @@ func TestAccessEnvVariables(t *testing.T) {
 	require.NoError(t, err)
 	require.Len(t, interceptor.Diagnostics, 2)
 	titles := []string{
-		"It is not permitted to access environment variables from plugins.",
+		"It is not permitted to access environment variables from plugins. DO_NOT_INCLUDE is not an accessible variable.",
 		"It is not permitted to access environment variables from plugins.",
 	}
-	require.Equal(t, titles, interceptor.GetTitles())
+	require.ElementsMatch(t, titles, interceptor.GetTitles())
+}
+
+func TestAccessAllowedEnvVariables(t *testing.T) {
+	var interceptor testpassinterceptor.TestPassInterceptor
+	pass := &analysis.Pass{
+		RootDir: filepath.Join("./"),
+		ResultOf: map[*analysis.Analyzer]interface{}{
+			sourcecode.Analyzer: filepath.Join("testdata", "access-env-allowed"),
+		},
+		Report: interceptor.ReportInterceptor(),
+	}
+
+	_, err := Analyzer.Run(pass)
+	require.NoError(t, err)
+	require.Len(t, interceptor.Diagnostics, 3)
+	titles := []string{
+		"It is not permitted to access environment variables from plugins. MY_VARIABLE is not an accessible variable.",
+		"It is not permitted to access environment variables from plugins. DO_NOT_INCLUDE is not an accessible variable.",
+		"It is not permitted to access environment variables from plugins.",
+	}
+	require.ElementsMatch(t, titles, interceptor.GetTitles())
 }
 
 func TestAccessFS(t *testing.T) {
@@ -48,7 +69,7 @@ func TestAccessFS(t *testing.T) {
 		"It is not permitted to access the file system.",
 		"It is not permitted to access the file system.",
 	}
-	require.Equal(t, titles, interceptor.GetTitles())
+	require.ElementsMatch(t, titles, interceptor.GetTitles())
 }
 
 func TestUseSyscall(t *testing.T) {
--- pkg/analysis/passes/coderules/testdata/access-env-allowed/main.go
+++ pkg/analysis/passes/coderules/testdata/access-env-allowed/main.go
@@ -8,9 +8,22 @@ import (
 func DoNotInclude() {
 	panic("This function should never be included in the binary.")
 
+	// NOT allowed
+	name := "MY_VARIABLE"
+	env := os.Getenv(name)
+
+	// GF_PLUGIN are allowed
+	name := "GF_PLUGIN_ALLOWED_ENV"
+	env := os.Getenv(name)
+
+	// NOT allowed
 	env := os.Getenv("DO_NOT_INCLUDE")
+
+	// GF_PLUGIN are allowed
+	env := os.Getenv("GF_PLUGIN_ALLOWED_ENV")
 	fmt.Println(env)
 
+	// NOT allowed
 	for _, e := range os.Environ() {
 		fmt.Println(e)
 	}
