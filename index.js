740e5f554625e850349326d800c90aea7d6880a9
Esteban Beltran <esteban.beltran@elastic.co>
2023-03-01 13:48:05 +0100

    Update package json to use binwrap

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ var path = require("path");
 var packageInfo = require(path.join(__dirname, "package.json"));
 var version = packageInfo.version;
 var root = `https://github.com/grafana/plugin-validator/releases/download/v${version}`;
-var root = "https://dl.bintray.com/me/myApp/" + version;
 
 module.exports = binwrap({
   dirname: __dirname,
-  binaries: ["myapp-cli"],
+  binaries: ["plugincheck2"],
   urls: {
-    "darwin-x64": root + "/mac-x64.tgz",
-    "linux-x64": root + "/linux-x64.tgz",
-    "win32-x64": root + "/win-i386.zip",
-    "win32-ia32": root + "/win-i386.zip",
+    // plugin-validator_0.7.3_darwin_amd64.tar.gz
+    "darwin-x64":
+      root + "/plugin-validator_" + version + "_darwin_amd64.tar.gz",
+    "darwin-arm64":
+      root + "/plugin-validator_" + version + "_darwin_arm64.tar.gz",
+    "linux-x64": root + "/plugin-validator_" + version + "_linux_amd64.tar.gz",
+    "linux-arm64":
+      root + "/plugin-validator_" + version + "_linux_arm64.tar.gz",
+    "win32-x64": root + "/plugin-validator_" + version + "_windows_amd64.zip",
+    "win32-ia32": root + "/plugin-validator_" + version + "_windows_386.zip",
+    "win32-arm64": root + "/plugin-validator_" + version + "_windows_arm64.zip",
   },
 });
