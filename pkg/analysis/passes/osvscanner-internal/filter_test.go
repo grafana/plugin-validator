9112fe0a1bc611a8dff5fe8ec683be17581877d9
Brian Gann <bkgann@gmail.com>
2023-03-13 21:49:18 -0400

    osv-scanner used as a library with more reliable filtering

---
+++ pkg/analysis/passes/osvscanner-internal/filter_test.go
@@ -0,0 +1,18 @@
+package OSVScannerInternal
+
+import (
+	"testing"
+)
+
+func TestFilterPackages(t *testing.T) {
+	/*
+		packages := models.VulnerabilityResults{
+			"d3-color",
+			"moment",
+		}
+
+		filteredResults := FilterOSVInternalResults(packages)
+		require.Len(t, filteredResults, 1) // should not have moment
+		require.Equal(t, "d3-color", filteredResults[0])
+	*/
+}
---
+++ pkg/analysis/passes/osvscanner-internal/testdata/golang/moderate/go.mod
@@ -0,0 +1,7 @@
+module github.com/grafana/grafana
+
+go 1.19
+
+require (
+	github.com/grafana/loki v1.6.2-0.20211015002020-7832783b1caa
+)
