7bec4ecce1dad42b0bdbb041f4c8902fdf7b293f
Brian Gann <bkgann@gmail.com>
2023-03-31 09:13:59 -0400

    add test for filter

--- pkg/analysis/passes/osvscanner/filter_test.go
+++ pkg/analysis/passes/osvscanner/filter_test.go
@@ -1,18 +1,36 @@
 package osvscanner
 
 import (
+	"path/filepath"
 	"testing"
+
+	"github.com/google/osv-scanner/pkg/models"
+	"github.com/stretchr/testify/require"
 )
 
 func TestFilterPackages(t *testing.T) {
-	/*
-		packages := models.VulnerabilityResults{
-			"d3-color",
-			"moment",
-		}
 
-		filteredResults := FilterOSVInternalResults(packages)
-		require.Len(t, filteredResults, 1) // should not have moment
-		require.Equal(t, "d3-color", filteredResults[0])
-	*/
+	packages := models.VulnerabilityResults{
+		Results: []models.PackageSource{
+			{
+				Source: models.SourceInfo{
+					Path: "d3-color",
+					Type: "lockfile",
+				},
+				Packages: []models.PackageVulns{},
+			},
+			{
+				Source: models.SourceInfo{
+					Path: "moment",
+					Type: "lockfile",
+				},
+				Packages: []models.PackageVulns{},
+			},
+		},
+	}
+
+	filteredResults := FilterOSVResults(packages, filepath.Join("testdata", "node", "critical", "yarn.lock"))
+	// should not have moment
+	require.Len(t, filteredResults.Results, 1)
+	require.Equal(t, "d3-color", filteredResults.Results[0].Source.Path)
 }
