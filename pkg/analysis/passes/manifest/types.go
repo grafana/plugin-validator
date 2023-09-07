0c1a268ed533c03e0fbddbd69d97ecb54bc2584b
Esteban Beltran <academo@users.noreply.github.com>
2023-09-07 15:37:11 +0200

    Add validation for a signature to not contain rootUrls (#136)
    * Add validation for a signature to not contain rootUrls
    
    * Update pkg/analysis/passes/manifest/manifest.go
    
    Co-authored-by: Giuseppe Guerra <giuseppe.guerra@grafana.com>
    
    ---------
    
    Co-authored-by: Giuseppe Guerra <giuseppe.guerra@grafana.com>

--- pkg/analysis/passes/manifest/types.go
+++ pkg/analysis/passes/manifest/types.go
@@ -1,5 +1,7 @@
 package manifest
 
 type ManifestFile struct {
-	Files map[string]string `json:"files"`
+	Files         map[string]string `json:"files"`
+	RootUrls      []string          `json:"rootUrls"`
+	SignatureType string            `json:"signatureType"`
 }
