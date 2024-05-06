2490511eee393fb84ed590bc1ea6608f4fb9af5d
Esteban Beltran <esteban@academo.me>
2024-05-06 16:43:42 +0200

    test error case for nestedmetadata

--- pkg/analysis/passes/nestedmetadata/nestedmetadata_test.go
+++ pkg/analysis/passes/nestedmetadata/nestedmetadata_test.go
@@ -63,3 +63,24 @@ func TestNestedMetadataWithNestedPluginJson(t *testing.T) {
 	require.Len(t, resultMap, 3)
 
 }
+
+func TestNestedMetadataWithNestedPluginJsonBadFormat(t *testing.T) {
+	var interceptor testpassinterceptor.TestPassInterceptor
+	pass := &analysis.Pass{
+		RootDir: filepath.Join("./"),
+		ResultOf: map[*analysis.Analyzer]interface{}{
+			archive.Analyzer: filepath.Join("testdata", "wrongnested"),
+		},
+		Report: interceptor.ReportInterceptor(),
+	}
+
+	result, err := Analyzer.Run(pass)
+
+	_, ok := result.(Metadatamap)
+	// should not be able to cast it
+	require.False(t, ok)
+
+	require.Error(t, err)
+	require.Len(t, interceptor.Diagnostics, 1)
+	require.Equal(t, "Invalid plugin.json in your archive.", interceptor.Diagnostics[0].Title)
+}
