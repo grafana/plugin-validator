6c6178316a4679c7669df56baafa6993dddac42d
Esteban Beltran <esteban@academo.me>
2024-05-07 13:45:03 +0200

    Update metadatapaths to validate nested plugins

--- pkg/analysis/passes/metadatapaths/metadatapaths_test.go
+++ pkg/analysis/passes/metadatapaths/metadatapaths_test.go
@@ -1,83 +1,70 @@
 package metadatapaths
 
 import (
-	"encoding/json"
 	"path/filepath"
 	"testing"
 
 	"github.com/grafana/plugin-validator/pkg/analysis"
-	"github.com/grafana/plugin-validator/pkg/analysis/passes/logos"
-	"github.com/grafana/plugin-validator/pkg/analysis/passes/metadata"
-	"github.com/grafana/plugin-validator/pkg/analysis/passes/screenshots"
+	"github.com/grafana/plugin-validator/pkg/analysis/passes/archive"
+	"github.com/grafana/plugin-validator/pkg/analysis/passes/nestedmetadata"
 	"github.com/grafana/plugin-validator/pkg/testpassinterceptor"
+	"github.com/grafana/plugin-validator/pkg/utils"
 	"github.com/stretchr/testify/require"
 )
 
 func TestMetadatapathsWithCorrectMetadata(t *testing.T) {
-	//prepare logosMeatadata
-	var logosMeatadata metadata.MetadataLogos
-	require.NoError(t, json.Unmarshal([]byte(`{"small": "img/logo.svg", "large": "img/logo.svg"}`), &logosMeatadata))
+	pluginJsonContent := []byte(`{
+    "id": "test-plugin-panel",
+    "info": {
+      "logos": {
+        "small": "img/logo.svg",
+        "large": "img/logo.svg"
+      }
+    }
+  }`)
 
-	//prepare screenshots
-	var screenshotsMetadata []metadata.MetadataScreenshots
-	require.NoError(t, json.Unmarshal([]byte(`[{"name": "test", "path": "img/screenshots.png"}]`), &screenshotsMetadata))
+	meta, err := utils.JsonToMetadata(pluginJsonContent)
+	require.NoError(t, err)
 
 	var interceptor testpassinterceptor.TestPassInterceptor
 	pass := &analysis.Pass{
 		RootDir: filepath.Join("./"),
 		ResultOf: map[*analysis.Analyzer]interface{}{
-			logos.Analyzer:       logosMeatadata,
-			screenshots.Analyzer: screenshotsMetadata,
+			archive.Analyzer: "testdata",
+			nestedmetadata.Analyzer: nestedmetadata.Metadatamap{
+				"plugin.json": meta,
+			},
 		},
 		Report: interceptor.ReportInterceptor(),
 	}
 
-	_, err := Analyzer.Run(pass)
+	_, err = Analyzer.Run(pass)
 	require.NoError(t, err)
 	require.Len(t, interceptor.Diagnostics, 0)
 }
 
 func TestMetadatapathsWithWrongLogoPath(t *testing.T) {
-	//prepare logosMeatadata
-	var logosMeatadata metadata.MetadataLogos
-	require.NoError(t, json.Unmarshal([]byte(`{"small": "/img/wrong-with-slash.svg", "large": "./img/wrong-with-dot.svg"}`), &logosMeatadata))
+	pluginJsonContent := []byte(`{
+    "id": "test-plugin-panel",
+    "info": {
+      "logos": {
+        "small": "./img/logo.svg",
+        "large": "./img/logo.svg"
+      }
+    }
+  }`)
 
-	//prepare screenshots
-	var screenshotsMetadata []metadata.MetadataScreenshots
-	require.NoError(t, json.Unmarshal([]byte(`[{"name": "test", "path": "img/screenshots.png"}]`), &screenshotsMetadata))
+	meta, err := utils.JsonToMetadata(pluginJsonContent)
+	require.NoError(t, err)
+
 	var interceptor testpassinterceptor.TestPassInterceptor
 	pass := &analysis.Pass{
 		RootDir: filepath.Join("./"),
 		ResultOf: map[*analysis.Analyzer]interface{}{
-			logos.Analyzer:       logosMeatadata,
-			screenshots.Analyzer: screenshotsMetadata,
-		},
-		Report: interceptor.ReportInterceptor(),
-	}
-
-	_, err := Analyzer.Run(pass)
-	require.NoError(t, err)
-	require.Len(t, interceptor.Diagnostics, 2)
-	require.Contains(t, interceptor.Diagnostics[0].Title, "plugin.json: relative small logo path should not start with")
-	require.Contains(t, interceptor.Diagnostics[1].Title, "plugin.json: relative large logo path should not start with")
-}
-
-func TestMetadatapathsWithWrongScreenshotPath(t *testing.T) {
-	//prepare logosMeatadata
-	var logosMeatadata metadata.MetadataLogos
-	err := json.Unmarshal([]byte(`{"small": "img/logo.svg", "large": "img/logo.svg"}`), &logosMeatadata)
-	require.NoError(t, err)
-
-	//prepare screenshots
-	var screenshotsMetadata []metadata.MetadataScreenshots
-	err = json.Unmarshal([]byte(`[{"name": "test", "path": "/img/wrong-with-slash.png"}, {"name": "test2", "path":"./img/wrong-with-dot"}]`), &screenshotsMetadata)
-	require.NoError(t, err)
-	var interceptor testpassinterceptor.TestPassInterceptor
-	pass := &analysis.Pass{
-		RootDir: filepath.Join("./"),
-		ResultOf: map[*analysis.Analyzer]interface{}{
-			logos.Analyzer:       logosMeatadata,
-			screenshots.Analyzer: screenshotsMetadata,
+			archive.Analyzer: "testdata",
+			nestedmetadata.Analyzer: nestedmetadata.Metadatamap{
+				"plugin.json": meta,
+			},
 		},
 		Report: interceptor.ReportInterceptor(),
 	}
@@ -85,6 +72,148 @@ func TestMetadatapathsWithWrongScreenshotPath(t *testing.T) {
 	_, err = Analyzer.Run(pass)
 	require.NoError(t, err)
 	require.Len(t, interceptor.Diagnostics, 2)
-	require.Contains(t, interceptor.Diagnostics[0].Title, "plugin.json: relative screenshot path should not start with")
-	require.Contains(t, interceptor.Diagnostics[1].Title, "plugin.json: relative screenshot path should not start with")
+	require.Contains(
+		t,
+		interceptor.Diagnostics[0].Title,
+		"plugin.json: relative small logo path should not start with",
+	)
+	require.Contains(
+		t,
+		interceptor.Diagnostics[1].Title,
+		"plugin.json: relative large logo path should not start with",
+	)
+}
+
+func TestMetadatapathsWithWrongScreenshotPath(t *testing.T) {
+	pluginJsonContent := []byte(`{
+    "id": "test-plugin-panel",
+    "info": {
+      "logos": {
+        "small": "img/logo.svg",
+        "large": "img/logo.svg"
+      },
+      "screenshots": [
+        {
+          "name": "test",
+          "path": "/img/logo.png"
+        },
+        {
+          "name": "test2",
+          "path": "./img/logo.png"
+        }
+        ]
+    }
+  }`)
+
+	meta, err := utils.JsonToMetadata(pluginJsonContent)
+	require.NoError(t, err)
+
+	var interceptor testpassinterceptor.TestPassInterceptor
+	pass := &analysis.Pass{
+		RootDir: filepath.Join("./"),
+		ResultOf: map[*analysis.Analyzer]interface{}{
+			archive.Analyzer: "testdata",
+			nestedmetadata.Analyzer: nestedmetadata.Metadatamap{
+				"plugin.json": meta,
+			},
+		},
+		Report: interceptor.ReportInterceptor(),
+	}
+
+	_, err = Analyzer.Run(pass)
+	require.NoError(t, err)
+	require.Len(t, interceptor.Diagnostics, 2)
+
+	require.Contains(
+		t,
+		interceptor.Diagnostics[0].Title,
+		"plugin.json: relative screenshot path should not start with",
+	)
+	require.Contains(
+		t,
+		interceptor.Diagnostics[1].Title,
+		"plugin.json: relative screenshot path should not start with",
+	)
+}
+
+func TestMeatadapathsWithCorrectNestedLogos(t *testing.T) {
+	pluginJsonContent := []byte(`{
+    "id": "test-plugin-panel",
+    "info": {
+      "logos": {
+        "small": "img/logo.svg",
+        "large": "img/logo.svg"
+      }
+    }
+  }`)
+
+	meta, err := utils.JsonToMetadata(pluginJsonContent)
+	require.NoError(t, err)
+
+	var interceptor testpassinterceptor.TestPassInterceptor
+	pass := &analysis.Pass{
+		RootDir: filepath.Join("./"),
+		ResultOf: map[*analysis.Analyzer]interface{}{
+			archive.Analyzer: "testdata",
+			nestedmetadata.Analyzer: nestedmetadata.Metadatamap{
+				"plugin.json":            meta,
+				"datasource/plugin.json": meta,
+			},
+		},
+		Report: interceptor.ReportInterceptor(),
+	}
+
+	_, err = Analyzer.Run(pass)
+	require.NoError(t, err)
+	require.Len(t, interceptor.Diagnostics, 0)
+}
+
+func TestMeatadapathsWithIncorrectNestedLogos(t *testing.T) {
+	pluginJsonContent := []byte(`{
+    "id": "test-plugin-panel",
+    "info": {
+      "logos": {
+        "small": "img/logo.svg",
+        "large": "img/logo.svg"
+      }
+    }
+  }`)
+
+	nestedPluginJsonContent := []byte(`{
+    "id": "test-plugin-panel",
+    "info": {
+      "logos": {
+        "small": "./img/logo.svg",
+        "large": "img/logo.svg"
+      }
+    }
+  }`)
+
+	meta, err := utils.JsonToMetadata(pluginJsonContent)
+	require.NoError(t, err)
+
+	wrongMeta, err := utils.JsonToMetadata(nestedPluginJsonContent)
+	require.NoError(t, err)
+
+	var interceptor testpassinterceptor.TestPassInterceptor
+	pass := &analysis.Pass{
+		RootDir: filepath.Join("./"),
+		ResultOf: map[*analysis.Analyzer]interface{}{
+			archive.Analyzer: "testdata",
+			nestedmetadata.Analyzer: nestedmetadata.Metadatamap{
+				"plugin.json":            meta,
+				"datasource/plugin.json": wrongMeta,
+			},
+		},
+		Report: interceptor.ReportInterceptor(),
+	}
+
+	_, err = Analyzer.Run(pass)
+	require.NoError(t, err)
+	require.Len(t, interceptor.Diagnostics, 1)
+	require.Contains(
+		t,
+		interceptor.Diagnostics[0].Title,
+		"plugin.json: relative small logo path should not start with",
+	)
 }
