416d62c2c64c431994e0628930942727b48fce4d
Will Browne <wbrowne@users.noreply.github.com>
2024-12-13 11:48:55 +0000

    License detect timeout (#289)
    * add 30s timeout for license detection
    
    * add specific err
    
    * fix string
    
    * Fix send on closed channel panic, pass error to main goroutine
    
    ---------
    
    Co-authored-by: Giuseppe Guerra <giuseppe@guerra.in>

--- pkg/analysis/passes/license/license.go
+++ pkg/analysis/passes/license/license.go
@@ -5,8 +5,10 @@ import (
 	"path/filepath"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/go-enry/go-license-detector/v4/licensedb"
+	"github.com/go-enry/go-license-detector/v4/licensedb/api"
 	"github.com/go-enry/go-license-detector/v4/licensedb/filer"
 
 	"github.com/grafana/plugin-validator/pkg/analysis"
@@ -15,9 +17,10 @@ import (
 )
 
 var (
-	licenseNotProvided     = &analysis.Rule{Name: "license-not-provided", Severity: analysis.Error}
-	licenseNotValid        = &analysis.Rule{Name: "license-not-valid", Severity: analysis.Error}
-	licenseWithGenericText = &analysis.Rule{
+	licenseNotProvided      = &analysis.Rule{Name: "license-not-provided", Severity: analysis.Error}
+	licenseNotValid         = &analysis.Rule{Name: "license-not-valid", Severity: analysis.Error}
+	licenseDetectionTimeout = &analysis.Rule{Name: "license-detection-timeout", Severity: analysis.Error}
+	licenseWithGenericText  = &analysis.Rule{
 		Name:     "license-with-generic-text",
 		Severity: analysis.Warning,
 	}
@@ -79,53 +82,76 @@ func run(pass *analysis.Pass) (interface{}, error) {
 		return nil, nil
 	}
 
-	// validate that the LICENSE file is parseable (go-license-detector lib method)
-	licenses, err := licensedb.Detect(f)
-	if err != nil {
-		pass.ReportResult(
-			pass.AnalyzerName,
-			licenseNotProvided,
-			"LICENSE file could not be parsed.",
-			"Could not parse the license file inside the plugin archive. Please make sure to include a valid license in your LICENSE file in your archive.",
-		)
-		return nil, nil
-	}
-
-	var foundLicense = false
-	for licenseName, licenseData := range licenses {
-		if licenseData.Confidence >= minRequiredConfidenceLevel && isValidLicense(licenseName) {
-			foundLicense = true
-			break
+	resultCh := make(chan map[string]api.Match, 1)
+	errCh := make(chan error, 1)
+	go func() {
+		// validate that the LICENSE file is parseable (go-license-detector lib method)
+		licenses, err := licensedb.Detect(f)
+		if err != nil {
+			errCh <- err
+			close(resultCh)
+			return
 		}
-	}
+		resultCh <- licenses
+		close(errCh)
+	}()
 
-	if !foundLicense {
+	select {
+	case err = <-errCh:
+		if err != nil {
+			pass.ReportResult(
+				pass.AnalyzerName,
+				licenseNotProvided,
+				"LICENSE file could not be parsed.",
+				"Could not parse the license file inside the plugin archive. Please make sure to include a valid license in your LICENSE file in your archive.",
+			)
+			return nil, nil
+		}
+	case licenses := <-resultCh:
+		var foundLicense = false
+		for licenseName, licenseData := range licenses {
+			if licenseData.Confidence >= minRequiredConfidenceLevel && isValidLicense(licenseName) {
+				foundLicense = true
+				break
+			}
+		}
+
+		if !foundLicense {
+			pass.ReportResult(
+				pass.AnalyzerName,
+				licenseNotValid,
+				"Valid license not found",
+				"The provided license is not compatible with Grafana plugins. Please refer to https://grafana.com/licensing/ for more information.",
+			)
+		} else if licenseNotProvided.ReportAll {
+			licenseNotProvided.Severity = analysis.OK
+			pass.ReportResult(pass.AnalyzerName, licenseNotProvided, "License found", "Found a valid license file inside the plugin archive.")
+		}
+
+		licenseContent, err := os.ReadFile(licenseFilePath)
+		if err != nil {
+			logme.Debugln("Could not read LICENSE file", err)
+			return nil, nil
+		}
+
+		licenseContentStr := string(licenseContent)
+		if strings.Contains(licenseContentStr, "{name of copyright owner}") ||
+			strings.Contains(licenseContentStr, "{yyyy}") {
+			pass.ReportResult(
+				pass.AnalyzerName,
+				licenseWithGenericText,
+				"License file contains generic text",
+				"Your current license file contains generic text from the license template. Please make sure to replace {name of copyright owner} and {yyyy} with the correct values in your LICENSE file.",
+			)
+		}
+	case <-time.After(time.Second * 30):
 		pass.ReportResult(
 			pass.AnalyzerName,
-			licenseNotValid,
-			"Valid license not found",
-			"The provided license is not compatible with Grafana plugins. Please refer to https://grafana.com/licensing/ for more information.",
+			licenseDetectionTimeout,
+			"License file detection timeout.",
+			"Could not detect the license file inside the plugin archive within 30s. Please make sure to include a valid license in your LICENSE file in your archive.",
 		)
-	} else if licenseNotProvided.ReportAll {
-		licenseNotProvided.Severity = analysis.OK
-		pass.ReportResult(pass.AnalyzerName, licenseNotProvided, "License found", "Found a valid license file inside the plugin archive.")
-	}
-
-	licenseContent, err := os.ReadFile(licenseFilePath)
-	if err != nil {
-		logme.Debugln("Could not read LICENSE file", err)
 		return nil, nil
-	}
-
-	licenseContentStr := string(licenseContent)
-	if strings.Contains(licenseContentStr, "{name of copyright owner}") ||
-		strings.Contains(licenseContentStr, "{yyyy}") {
-		pass.ReportResult(
-			pass.AnalyzerName,
-			licenseWithGenericText,
-			"License file contains generic text",
-			"Your current license file contains generic text from the license template. Please make sure to replace {name of copyright owner} and {yyyy} with the correct values in your LICENSE file.",
-		)
 	}
 
 	return nil, nil
