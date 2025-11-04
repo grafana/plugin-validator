e85dc042992fe94c398cbbed10b93dc293bd9bc8
Giuseppe Guerra <giuseppe@guerra.in>
2025-11-04 15:58:53 +0100

    add comments

--- pkg/analysis/output/output.go
+++ pkg/analysis/output/output.go
@@ -8,21 +8,34 @@ import (
 	"github.com/grafana/plugin-validator/pkg/analysis"
 )
 
+// Marshaler is an interface for encoding the analysis results into bytes.
+// Each implementation outputs the analysis results in a different format.
 type Marshaler interface {
+	// Marshal encodes the diagnostics data in the format implemented by the marshaler.
 	Marshal(data analysis.Diagnostics) ([]byte, error)
 }
 
+// marshalerFunc is an adapter for using normal functions as Marshaler.
 type marshalerFunc func(data analysis.Diagnostics) ([]byte, error)
 
+// Marshal marshals the diagnostics data using the function.
 func (f marshalerFunc) Marshal(data analysis.Diagnostics) ([]byte, error) {
 	return f(data)
 }
 
+// jsonMarshaler is a Marshaler that outputs to JSON format.
 type jsonMarshaler struct {
-	id      string
+	// Additional fields used for JSON output
+
+	// id is the plugin ID
+	id string
+
+	// version is the plugin version
 	version string
 }
 
+// NewJSONMarshaler returns a new Marshaler that outputs the diagnostics data in JSON format.
+// This marshaler requires additional plugin id and plugin version arguments.
 func NewJSONMarshaler(id string, version string) Marshaler {
 	return jsonMarshaler{id, version}
 }
@@ -33,6 +46,8 @@ type jsonOutput struct {
 	Diagnostics analysis.Diagnostics `json:"plugin-validator"`
 }
 
+// Marshal marshals the diagnostics data in JSON format.
+// The additional id and version fields are taken from the marshaler itself.
 func (j jsonMarshaler) Marshal(data analysis.Diagnostics) ([]byte, error) {
 	return json.MarshalIndent(jsonOutput{
 		ID:          j.id,
@@ -41,6 +56,7 @@ func (j jsonMarshaler) Marshal(data analysis.Diagnostics) ([]byte, error) {
 	}, "", "  ")
 }
 
+// MarshalCLI is a Marshaler that returns the diagnostics data in a human-readable format, for CLI usage.
 var MarshalCLI = marshalerFunc(func(data analysis.Diagnostics) ([]byte, error) {
 	var buf bytes.Buffer
 	for name := range data {
@@ -74,6 +90,11 @@ var MarshalCLI = marshalerFunc(func(data analysis.Diagnostics) ([]byte, error) {
 	return buf.Bytes(), nil
 })
 
+// ExitCode returns the exit code of the CLI program.
+// It returns:
+//   - 1 if there's an error;
+//   - 1 if there's a warning AND strict is true;
+//   - 0 in all other cases.
 func ExitCode(strict bool, diags analysis.Diagnostics) int {
 	for _, ds := range diags {
 		for _, d := range ds {
