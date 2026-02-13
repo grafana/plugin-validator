c899813272c0520ae8803538d76c0b3841f15d43
Giuseppe Guerra <giuseppe@guerra.in>
2026-02-13 12:34:35 +0100

    add TestGrafanaDependencyParse

---
+++ pkg/analysis/passes/grafanadependency/grafanadependency_test.go
@@ -0,0 +1,28 @@
+package grafanadependency
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/mod/semver"
+)
+
+func TestGrafanaDependencyParse(t *testing.T) {
+	for _, tc := range []struct {
+		name       string
+		dependency string
+		expPre     string
+	}{
+		{"no pre-release", ">=12.4.0", ""},
+		{"no pre-release with space", ">= 12.4.0", ""},
+		{"zero pre-release", ">=12.4.0-0", "0"},
+		{"zero pre-release with space", ">= 12.4.0-0", "0"},
+		{"non-zero pre-release", ">=12.4.0-01189998819991197253", "01189998819991197253"},
+		{"non-zero pre-release with space", ">= 12.4.0-01189998819991197253", "01189998819991197253"},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			pre := semver.Prerelease(">=12.4.0")
+			require.Empty(t, pre)
+		})
+	}
+}
