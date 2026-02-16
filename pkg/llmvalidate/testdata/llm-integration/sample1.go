118cbd6da5ef442b7c320c042ce4b798bc5c0eb7
Esteban Beltran <sergies@gmail.com>
2026-02-16 18:05:30 +0100

    add basic integration tests

---
+++ pkg/llmvalidate/testdata/llm-integration/sample1.go
@@ -0,0 +1,8 @@
+package test
+
+// MAGIC_MARKER_BANANA_12345
+// This marker is used for integration testing
+
+func HelloWorld() string {
+	return "hello world"
+}
---
+++ pkg/llmvalidate/testdata/llm-integration/sample2.go
@@ -0,0 +1,7 @@
+package test
+
+// Just a normal file without the magic marker
+
+func GoodbyeWorld() string {
+	return "goodbye world"
+}
